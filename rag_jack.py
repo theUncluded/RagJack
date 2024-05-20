fb81f16ce21cd786786bb075c57b1eaec5a5bbeb
Rich Palestri <92674403+theUncluded@users.noreply.github.com>
2024-05-20 13:09:59 -0400

    Update rag_jack.py
    cleaned up redundant imports

--- rag_jack.py
+++ rag_jack.py
@@ -1,14 +1,10 @@
 # Import libraries.
 import getpass
-import gradio as gr
 import os
 import pprint
 import requests
 import sys
-import weaviate
 
-
-from gradio.themes.base import Base
 from icecream import ic
 from langchain.callbacks.tracers import ConsoleCallbackHandler
 from langchain.chat_models import ChatOpenAI
@@ -21,9 +17,7 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Weaviate
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-from weaviate.embedded import EmbeddedOptions
 
 os.environ["OPEN_API_KEY"] = getpass.getpass()
 
